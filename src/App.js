fd740bade0275cd7905dc1532309cae2ed428c7f
hanna sonnenberg <sonnenberghanna@googlemail.com>
2021-02-06 16:56:19 +0100

    setup for api call in App.js and basic styling in index.css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import './index.css';
 
-//create App component
+// create App component
 
 const App = () => {
+  // Authentification
+  const API_KEY = 'a043bd10eceb45ce8297913854a17649';
+
+  // API Request example search recipes by ingredients
+  // const exampleReq = 'https://api.spoonacular.com/recipes/findByIngredients?ingredients=apples,+flour,+sugar&number=2';
+
+  useEffect(() => {
+    // console.log('Effect has been run');
+    // run the function getRecipes to fetch the data from the Api
+    getRecipes();
+  }, []);
+
+  // function that handles the Get Request
+  const getRecipes = async () => {
+    try {
+      // sends request
+      const response = await fetch(`https://api.spoonacular.com/recipes/findByIngredients?apiKey=${API_KEY}&ingredients=apples,+flour,+sugar&number=2`);
+      // handles response if successful
+      if (response.ok) {
+        const data = await response.json();
+        // Code to execute with data
+        console.log(data);
+      }
+      // handles response if unseccessful
+      throw new Error('Request failed');
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return(
-    <div>
-      <h1>Hello React</h1>
+    <div className="App">
+      <form className = "search-form">
+        <input className="search-bar" type="text"/>
+        <button className="search-button" type="submit">Search</button>
+      </form>
     </div>
   )
 }
