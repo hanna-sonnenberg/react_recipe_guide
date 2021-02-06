183abc832c817a3673302fa731a0c96ad7b50bd8
hanna sonnenberg <sonnenberghanna@googlemail.com>
2021-02-06 21:07:55 +0100

    added api key in env.local file and gitignore, added Recipe component to display fetched data

---
+++ src/Recipe.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Recipe = () => {
+    return(
+        <div>
+            <h1>Title</h1>
+            <img src="" alt=""/>
+            <div>
+                <h5>Ingredients</h5>
+                <h5>Preparation</h5>
+            </div>
+        </div>
+    )
+
+}
+
+export default Recipe;
